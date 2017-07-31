b5cbcf89b5331f028b3c2770c2a26de65fd032a2
poojan Shah <poojan@poojanshah.com>
2017-07-31 12:24:19 +0100

    crashlytics

--- app/src/main/java/com/example/shahp/finalproject/MainActivity.java
+++ app/src/main/java/com/example/shahp/finalproject/MainActivity.java
@@ -12,14 +12,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,65 +37,17 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+
+                drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-
-                // Customize map with markers, polylines, etc.
-
-            }
-        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/example/shahp/finalproject/MyApplication.java
+++ app/src/main/java/com/example/shahp/finalproject/MyApplication.java
@@ -2,7 +2,9 @@ package com.example.shahp.finalproject;
 
 import android.app.Application;
 
-import com.mapbox.mapboxsdk.Mapbox;
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by shahp on 28/07/2017.
@@ -13,8 +15,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        // Mapbox Access token
-        Mapbox.getInstance(getApplicationContext(), "pk.eyJ1IjoicG9vamFuIiwiYSI6ImNqNW1oMDlzNzBxajEyeHJ3N28yanVqNzQifQ.o2l67-t1c_OLzKcPIsr68g");
+        Fabric.with(this, new Crashlytics());
     }
 }
