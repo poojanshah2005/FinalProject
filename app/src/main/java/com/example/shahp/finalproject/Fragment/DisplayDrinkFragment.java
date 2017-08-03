4641c3e46e18733dd779c8193a54c1aa99fd7722
poojan Shah <poojan@poojanshah.com>
2017-08-03 17:15:55 +0100

    setMinimumHeight

--- app/src/main/java/com/example/shahp/finalproject/Fragment/DisplayDrinkFragment.java
+++ app/src/main/java/com/example/shahp/finalproject/Fragment/DisplayDrinkFragment.java
@@ -80,178 +80,197 @@ public class DisplayDrinkFragment extends Fragment {
         tvAlc.setText("Alcoholic: " + drink.getStrAlcoholic());
         tvInstructions.setText("Instructions: " + drink.getStrInstructions());
 
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-            TableRow row1= new TableRow(getContext());
-            TableRow row2= new TableRow(getContext());
-            TableRow row3= new TableRow(getContext());
-            TableRow row4= new TableRow(getContext());
-            TableRow row5= new TableRow(getContext());
-            TableRow row6= new TableRow(getContext());
-            TableRow row7= new TableRow(getContext());
-            TableRow row8= new TableRow(getContext());
-            TableRow row9= new TableRow(getContext());
-            TableRow row10= new TableRow(getContext());
-            TableRow row11= new TableRow(getContext());
-            TableRow row12= new TableRow(getContext());
-            TableRow row13= new TableRow(getContext());
-            TableRow row14= new TableRow(getContext());
-            TableRow row15= new TableRow(getContext());
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        TableRow row1= new TableRow(getContext());
+        TableRow row2= new TableRow(getContext());
+        TableRow row3= new TableRow(getContext());
+        TableRow row4= new TableRow(getContext());
+        TableRow row5= new TableRow(getContext());
+        TableRow row6= new TableRow(getContext());
+        TableRow row7= new TableRow(getContext());
+        TableRow row8= new TableRow(getContext());
+        TableRow row9= new TableRow(getContext());
+        TableRow row10= new TableRow(getContext());
+        TableRow row11= new TableRow(getContext());
+        TableRow row12= new TableRow(getContext());
+        TableRow row13= new TableRow(getContext());
+        TableRow row14= new TableRow(getContext());
+        TableRow row15= new TableRow(getContext());
+        int a = 35;
 
 
-            row1.setLayoutParams(lp);
-            row2.setLayoutParams(lp);
-            row3.setLayoutParams(lp);
-            row4.setLayoutParams(lp);
-            row5.setLayoutParams(lp);
-            row6.setLayoutParams(lp);
-            row7.setLayoutParams(lp);
-            row8.setLayoutParams(lp);
-            row9.setLayoutParams(lp);
-            row10.setLayoutParams(lp);
-            row11.setLayoutParams(lp);
-            row12.setLayoutParams(lp);
-            row13.setLayoutParams(lp);
-            row14.setLayoutParams(lp);
-            row15.setLayoutParams(lp);
 
 
-            TextView tv1I = new TextView(getContext());
-            TextView tv1Q = new TextView(getContext());
-            TextView tv2I = new TextView(getContext());
-            TextView tv2Q = new TextView(getContext());
-            TextView tv3I = new TextView(getContext());
-            TextView tv3Q = new TextView(getContext());
-            TextView tv4I = new TextView(getContext());
-            TextView tv4Q = new TextView(getContext());
-            TextView tv5I = new TextView(getContext());
-            TextView tv5Q = new TextView(getContext());
-            TextView tv6I = new TextView(getContext());
-            TextView tv6Q = new TextView(getContext());
-            TextView tv7I = new TextView(getContext());
-            TextView tv7Q = new TextView(getContext());
-            TextView tv8I = new TextView(getContext());
-            TextView tv8Q = new TextView(getContext());
-            TextView tv9I = new TextView(getContext());
-            TextView tv9Q = new TextView(getContext());
-            TextView tv10I = new TextView(getContext());
-            TextView tv10Q = new TextView(getContext());
-            TextView tv11I = new TextView(getContext());
-            TextView tv11Q = new TextView(getContext());
-            TextView tv12I = new TextView(getContext());
-            TextView tv12Q = new TextView(getContext());
-            TextView tv13I = new TextView(getContext());
-            TextView tv13Q = new TextView(getContext());
-            TextView tv14I = new TextView(getContext());
-            TextView tv14Q = new TextView(getContext());
-            TextView tv15I = new TextView(getContext());
-            TextView tv15Q = new TextView(getContext());
+        row1.setLayoutParams(lp);
+        row2.setLayoutParams(lp);
+        row3.setLayoutParams(lp);
+        row4.setLayoutParams(lp);
+        row5.setLayoutParams(lp);
+        row6.setLayoutParams(lp);
+        row7.setLayoutParams(lp);
+        row8.setLayoutParams(lp);
+        row9.setLayoutParams(lp);
+        row10.setLayoutParams(lp);
+        row11.setLayoutParams(lp);
+        row12.setLayoutParams(lp);
+        row13.setLayoutParams(lp);
+        row14.setLayoutParams(lp);
+        row15.setLayoutParams(lp);
 
-            tv1I.setText(drink.getStrIngredient1());
-            tv1Q.setText(drink.getStrMeasure1());
-
-            tv2I.setText(drink.getStrIngredient2());
-            tv2Q.setText(drink.getStrMeasure2());
-
-            tv3I.setText(drink.getStrIngredient3());
-            tv3Q.setText(drink.getStrMeasure3());
-
-            tv4I.setText(drink.getStrIngredient4());
-            tv4Q.setText(drink.getStrMeasure4());
-
-            tv5I.setText(drink.getStrIngredient5());
-            tv5Q.setText(drink.getStrMeasure5());
-
-            tv6I.setText(drink.getStrIngredient6());
-            tv6Q.setText(drink.getStrMeasure6());
-
-            tv7I.setText(drink.getStrIngredient7());
-            tv7Q.setText(drink.getStrMeasure7());
-
-            tv8I.setText(drink.getStrIngredient8());
-            tv8Q.setText(drink.getStrMeasure8());
-
-            tv9I.setText(drink.getStrIngredient9());
-            tv9Q.setText(drink.getStrMeasure9());
-
-            tv10I.setText(drink.getStrIngredient10());
-            tv10Q.setText(drink.getStrMeasure10());
-
-            tv11I.setText(drink.getStrIngredient11());
-            tv11Q.setText(drink.getStrMeasure11());
-
-            tv12I.setText(drink.getStrIngredient12());
-            tv12Q.setText(drink.getStrMeasure12());
-
-            tv13I.setText(drink.getStrIngredient13());
-            tv13Q.setText(drink.getStrMeasure13());
-
-            tv14I.setText(drink.getStrIngredient14());
-            tv14Q.setText(drink.getStrMeasure14());
-
-            tv15I.setText(drink.getStrIngredient15());
-            tv15Q.setText(drink.getStrMeasure15());
+        row1.setMinimumHeight(a);
+        row2.setMinimumHeight(a);
+        row3.setMinimumHeight(a);
+        row4.setMinimumHeight(a);
+        row5.setMinimumHeight(a);
+        row6.setMinimumHeight(a);
+        row7.setMinimumHeight(a);
+        row8.setMinimumHeight(a);
+        row9.setMinimumHeight(a);
+        row10.setMinimumHeight(a);
+        row11.setMinimumHeight(a);
+        row12.setMinimumHeight(a);
+        row13.setMinimumHeight(a);
+        row14.setMinimumHeight(a);
+        row15.setMinimumHeight(a);
 
 
-            row1.addView(tv1I);
-            row1.addView(tv1Q);
+        TextView tv1I = new TextView(getContext());
+        TextView tv1Q = new TextView(getContext());
+        TextView tv2I = new TextView(getContext());
+        TextView tv2Q = new TextView(getContext());
+        TextView tv3I = new TextView(getContext());
+        TextView tv3Q = new TextView(getContext());
+        TextView tv4I = new TextView(getContext());
+        TextView tv4Q = new TextView(getContext());
+        TextView tv5I = new TextView(getContext());
+        TextView tv5Q = new TextView(getContext());
+        TextView tv6I = new TextView(getContext());
+        TextView tv6Q = new TextView(getContext());
+        TextView tv7I = new TextView(getContext());
+        TextView tv7Q = new TextView(getContext());
+        TextView tv8I = new TextView(getContext());
+        TextView tv8Q = new TextView(getContext());
+        TextView tv9I = new TextView(getContext());
+        TextView tv9Q = new TextView(getContext());
+        TextView tv10I = new TextView(getContext());
+        TextView tv10Q = new TextView(getContext());
+        TextView tv11I = new TextView(getContext());
+        TextView tv11Q = new TextView(getContext());
+        TextView tv12I = new TextView(getContext());
+        TextView tv12Q = new TextView(getContext());
+        TextView tv13I = new TextView(getContext());
+        TextView tv13Q = new TextView(getContext());
+        TextView tv14I = new TextView(getContext());
+        TextView tv14Q = new TextView(getContext());
+        TextView tv15I = new TextView(getContext());
+        TextView tv15Q = new TextView(getContext());
 
-            row2.addView(tv2I);
-            row2.addView(tv2Q);
+        tv1I.setText(drink.getStrIngredient1());
+        tv1Q.setText(drink.getStrMeasure1());
 
-            row3.addView(tv3I);
-            row3.addView(tv3Q);
+        tv2I.setText(drink.getStrIngredient2());
+        tv2Q.setText(drink.getStrMeasure2());
 
-            row4.addView(tv4I);
-            row4.addView(tv4Q);
+        tv3I.setText(drink.getStrIngredient3());
+        tv3Q.setText(drink.getStrMeasure3());
 
-            row5.addView(tv5I);
-            row5.addView(tv5Q);
+        tv4I.setText(drink.getStrIngredient4());
+        tv4Q.setText(drink.getStrMeasure4());
 
-            row6.addView(tv6I);
-            row6.addView(tv6Q);
+        tv5I.setText(drink.getStrIngredient5());
+        tv5Q.setText(drink.getStrMeasure5());
 
-            row7.addView(tv7I);
-            row7.addView(tv7Q);
+        tv6I.setText(drink.getStrIngredient6());
+        tv6Q.setText(drink.getStrMeasure6());
 
-            row8.addView(tv8I);
-            row8.addView(tv8Q);
+        tv7I.setText(drink.getStrIngredient7());
+        tv7Q.setText(drink.getStrMeasure7());
 
-            row9.addView(tv9I);
-            row9.addView(tv9Q);
+        tv8I.setText(drink.getStrIngredient8());
+        tv8Q.setText(drink.getStrMeasure8());
 
-            row10.addView(tv10I);
-            row10.addView(tv10Q);
+        tv9I.setText(drink.getStrIngredient9());
+        tv9Q.setText(drink.getStrMeasure9());
 
-            row11.addView(tv11I);
-            row11.addView(tv11Q);
+        tv10I.setText(drink.getStrIngredient10());
+        tv10Q.setText(drink.getStrMeasure10());
 
-            row12.addView(tv12I);
-            row12.addView(tv12Q);
+        tv11I.setText(drink.getStrIngredient11());
+        tv11Q.setText(drink.getStrMeasure11());
 
-            row13.addView(tv13I);
-            row13.addView(tv13Q);
+        tv12I.setText(drink.getStrIngredient12());
+        tv12Q.setText(drink.getStrMeasure12());
 
-            row14.addView(tv14I);
-            row14.addView(tv14Q);
+        tv13I.setText(drink.getStrIngredient13());
+        tv13Q.setText(drink.getStrMeasure13());
 
-            row15.addView(tv15I);
-            row15.addView(tv15Q);
+        tv14I.setText(drink.getStrIngredient14());
+        tv14Q.setText(drink.getStrMeasure14());
 
-            DrinkIngs.addView(row1);
-            DrinkIngs.addView(row2);
-            DrinkIngs.addView(row3);
-            DrinkIngs.addView(row4);
-            DrinkIngs.addView(row5);
-            DrinkIngs.addView(row6);
-            DrinkIngs.addView(row7);
-            DrinkIngs.addView(row8);
-            DrinkIngs.addView(row9);
-            DrinkIngs.addView(row10);
-            DrinkIngs.addView(row11);
-            DrinkIngs.addView(row12);
-            DrinkIngs.addView(row13);
-            DrinkIngs.addView(row14);
-            DrinkIngs.addView(row15);
+        tv15I.setText(drink.getStrIngredient15());
+        tv15Q.setText(drink.getStrMeasure15());
+
+
+        row1.addView(tv1I);
+        row1.addView(tv1Q);
+
+        row2.addView(tv2I);
+        row2.addView(tv2Q);
+
+        row3.addView(tv3I);
+        row3.addView(tv3Q);
+
+        row4.addView(tv4I);
+        row4.addView(tv4Q);
+
+        row5.addView(tv5I);
+        row5.addView(tv5Q);
+
+        row6.addView(tv6I);
+        row6.addView(tv6Q);
+
+        row7.addView(tv7I);
+        row7.addView(tv7Q);
+
+        row8.addView(tv8I);
+        row8.addView(tv8Q);
+
+        row9.addView(tv9I);
+        row9.addView(tv9Q);
+
+        row10.addView(tv10I);
+        row10.addView(tv10Q);
+
+        row11.addView(tv11I);
+        row11.addView(tv11Q);
+
+        row12.addView(tv12I);
+        row12.addView(tv12Q);
+
+        row13.addView(tv13I);
+        row13.addView(tv13Q);
+
+        row14.addView(tv14I);
+        row14.addView(tv14Q);
+
+        row15.addView(tv15I);
+        row15.addView(tv15Q);
+
+        DrinkIngs.addView(row1);
+        DrinkIngs.addView(row2);
+        DrinkIngs.addView(row3);
+        DrinkIngs.addView(row4);
+        DrinkIngs.addView(row5);
+        DrinkIngs.addView(row6);
+        DrinkIngs.addView(row7);
+        DrinkIngs.addView(row8);
+        DrinkIngs.addView(row9);
+        DrinkIngs.addView(row10);
+        DrinkIngs.addView(row11);
+        DrinkIngs.addView(row12);
+        DrinkIngs.addView(row13);
+        DrinkIngs.addView(row14);
+        DrinkIngs.addView(row15);
 
         try {
             String imageLink = (String) drink.getStrDrinkThumb();
